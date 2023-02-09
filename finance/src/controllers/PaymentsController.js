349b9914da4982ea870779896957ae04746f6363
Almeida Berguem Paula <n917833@gruposantander.com>
2023-02-09 15:44:51 -0300

    feat: get detail payments

--- finance/src/controllers/PaymentsController.js
+++ finance/src/controllers/PaymentsController.js
@@ -20,6 +20,28 @@ class PaymentsController {
         return res.status(500).json(error.message)
     }
    }
+   static async getDetailPayment(req, res){
+    const { id } = req.params
+    try {
+        const getPaymentByID = await database.Payments.findOne({
+            where: {
+                id: Number(id)
+            }
+        })
+
+        if(!getPaymentByID){
+            return res.status(200).json({message: 'usuário não encontrado'})    
+        }
+
+        const {...dataPayments} = getPaymentByID
+        const {cvv: _, ...datas} = dataPayments.dataValues
+
+        return res.status(201).json(datas)
+    } catch (error) {
+        return res.status(500).json(error.message)
+    }
+   }
+   
 }
 
 module.exports = PaymentsController
