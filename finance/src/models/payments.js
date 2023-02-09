86462aa60130b51a8b94f2321fe4a9272af92cc2
Almeida Berguem Paula <n917833@gruposantander.com>
2023-02-09 14:44:43 -0300

    fix: includes validate

--- finance/src/models/payments.js
+++ finance/src/models/payments.js
@@ -1,26 +1,54 @@
-'use strict';
-const { Model } = require('sequelize');
+'use-strict'
+
 module.exports = (sequelize, DataTypes) => {
-  class Payments extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+  const Payments = sequelize.define('Payments', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    valor: {
+      type: DataTypes.DECIMAL(10,2),
+      validate: {
+        min: 0
+      }
+    },
+    nome: {
+      type: DataTypes.STRING,
+      validate: {
+        funcaoValidadora: function(dado){
+          if (dado.length < 3) throw new Error('o campo nome deve ter mais de 3 caracteres')
+        },
+        isAlpha: true
+      }
+    },
+    numeroCartao: {
+      type: DataTypes.STRING,
+      validate: {
+        isCreditCard: true
+      }
+    },
+    dataExpiracao: {
+      type: DataTypes.STRING,
+      validate: {
+        is: /(\d{4})-(0[1-9]|1[0-2])$/gm
+      }
+    },
+    cvv: {
+      type: DataTypes.STRING,
+      validate: {
+        isNumeric: true,
+        len: [3]
+      }
+    },
+    status: {
+      type: DataTypes.STRING,
+      validate: {
+        isIn:  [['CRIADO', 'CONFIRMADO', 'CANCELADO']]
+      },
+      defaultValue: "CRIADO"
     }
-  }
-  Payments.init({
-    valor: DataTypes.DECIMAL,
-    nome: DataTypes.STRING,
-    numeroCartao: DataTypes.STRING,
-    dataExpiracao: DataTypes.STRING,
-    cvv: DataTypes.STRING,
-    status: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Payments',
   });
+
   return Payments;
 };
