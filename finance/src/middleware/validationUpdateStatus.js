1aa4d7d18d8f7d98acf3527b62e105358e0c7a4e
Almeida Berguem Paula <n917833@gruposantander.com>
2023-02-10 15:05:37 -0300

    validation status

--- finance/src/middleware/validationUpdateStatus.js
+++ finance/src/middleware/validationUpdateStatus.js
@@ -3,14 +3,11 @@ const database = require('../models');
 const verifyStatus = async (req, res, next) => {
     const { id } = req.params
     try {
-        const {status} = await database.Payments.findById(id)
-        if (status === 'CONFIRMADO') {
-            return res.status(405).json({ message: 'o status não pode ser alterado!' });
-          }
-        if (status === 'CANCELADO') {
-            return res.status(405).json({ message: 'o status não pode ser alterado' });
-        }
-          next();
+        const {status} = await database.Payments.findOne({where: {id: Number(id)}});
+        
+        if (status === 'CONFIRMADO' || status === 'CANCELADO') return res.status(405).json({ message: 'o status não pode ser alterado!' });
+        
+        next();
     } catch (error) {
         return res.status(500).json(error.message)
     }
