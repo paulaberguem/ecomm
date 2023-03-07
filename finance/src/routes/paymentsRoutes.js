00354b5bcbf4c534d9dc233b7aad7acf60017d74
Almeida Berguem Paula <n917833@gruposantander.com>
2023-03-07 15:09:49 -0300

    fix: eslint

--- finance/src/routes/paymentsRoutes.js
+++ finance/src/routes/paymentsRoutes.js
@@ -1,14 +1,17 @@
 const { Router } = require('express');
+// eslint-disable-next-line import/extensions
 const PaymentsController = require('../controllers/PaymentsController');
+// eslint-disable-next-line import/extensions
 const updateValidation = require('../middleware/validationUpdateStatus');
+// eslint-disable-next-line import/extensions
 const idValidation = require('../middleware/idValidation');
 
 const router = Router();
 
-router.get('/payments', PaymentsController.getPayments)
-router.post('/payments', PaymentsController.insertPayments)
-router.get('/payments/:id', idValidation.verifyID, PaymentsController.getDetailPayment)
-router.patch('/payments/:id/confirm', idValidation.verifyID, updateValidation.verifyStatus, PaymentsController.updateStatusPayment)
-router.patch('/payments/:id/cancel', idValidation.verifyID, updateValidation.verifyStatus, PaymentsController.updateStatusPayment)
+router.get('/payments', PaymentsController.getPayments);
+router.post('/payments', PaymentsController.insertPayments);
+router.get('/payments/:id', idValidation.verifyID, PaymentsController.getDetailPayment);
+router.patch('/payments/:id/confirm', idValidation.verifyID, updateValidation.verifyStatus, PaymentsController.updateStatusPayment);
+router.patch('/payments/:id/cancel', idValidation.verifyID, updateValidation.verifyStatus, PaymentsController.updateStatusPayment);
 
 module.exports = router;
