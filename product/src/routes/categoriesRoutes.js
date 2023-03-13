794ff4e6f80f69038d9fa990e2fc3b4cbd40b7d1
Almeida Berguem Paula <n917833@gruposantander.com>
2023-03-13 15:59:19 -0300

    feat: authentication

--- product/src/routes/categoriesRoutes.js
+++ product/src/routes/categoriesRoutes.js
@@ -1,14 +1,15 @@
 import express from 'express';
 import CategoryController from '../controllers/categoriesController.js';
+import { bearerStrategyAuth } from '../authorization/bearerEstrategia.js';
 
 const router = express.Router();
 
 router
   .get('/categories', CategoryController.getCategories)
-  .post('/admin/categories', CategoryController.insertCategory)
   .get('/categories/:id', CategoryController.getCategoryById)
-  .put('/admin/categories/:id', CategoryController.updateCategory)
-  .delete('/admin/categories/:id', CategoryController.deleteCategory)
-  .patch('/admin/categories/:id', CategoryController.updateStatusCategory);
+  .post('/admin/categories', bearerStrategyAuth, CategoryController.insertCategory)
+  .put('/admin/categories/:id', bearerStrategyAuth, CategoryController.updateCategory)
+  .delete('/admin/categories/:id', bearerStrategyAuth, CategoryController.deleteCategory)
+  .patch('/admin/categories/:id', bearerStrategyAuth, CategoryController.updateStatusCategory);
 
 export default router;
